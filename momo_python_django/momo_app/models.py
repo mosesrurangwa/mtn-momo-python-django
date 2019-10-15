7357e4e6723aee81013aff3aab30485541d2f059
rurangwa <rurangwa@awamo.com>
2019-10-15 23:08:33 +0300

    Adds MomoRequest database model

--- momo_python_django/momo_app/models.py
+++ momo_python_django/momo_app/models.py
@@ -1,3 +1,14 @@
+import os
 from django.db import models
 
 # Create your models here.
+class MomoRequest(models.Model):
+    msisdn = models.CharField(max_length=12, blank=False)
+    amount = models.CharField(max_length=250, blank=False)
+    external_id = models.CharField(max_length=250, blank=False)
+    payee_note = models.TextField(blank=False)
+    payee_message = models.TextField(blank=False)
+    currency = models.CharField(max_length=25, blank=False)
+    transaction_ref = models.TextField()
+    request_status = models.CharField(max_length=250)
+    lastest_api_response = models.TextField()
